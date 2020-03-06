e8fbaedb2a69e62b66f293b96831fbf2a0c0ca84
Vagahbond <56230065+Vagahbond@users.noreply.github.com>
2020-03-06 10:49:09 +0100

    Include chromaprint.h instead of chroma.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
-#include <chroma.h>
+#include <chromaprint.h>
+#include <iostream>
 
 using namespace chromaprint;
 
 int main()
 {
     std::cout << "Hello, Chromaprint!" << std::endl;
+    std::cout << "Version: " << chromaprint_get_version << std::endl;
 
     return 0;
 }
